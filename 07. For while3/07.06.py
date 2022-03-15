a79bc17878a6cf4534660bdd3f5b53c804155658
Руди <rudykhabib@mail.ru>
2022-03-15 17:25:51 +0300

    del

--- 07. For while3/07.06.py
+++
@@ -1,7 +0,0 @@
-paper = float(input('Введите сторону листа: '))
-convert = 12
-counter = 0
-while paper > convert:
-    paper /= 2
-    counter += 2
-print(counter)
--- 07. For while3/07.07.py
+++
@@ -1,6 +0,0 @@
-scholarship = int(input('Введите стипендию: '))
-payment = int(input('Введите расходы на проживание: '))
-credit = 0
-for i in range(1, 10+1):
-    credit += payment * (1.03 ** (i - 1))
-print('У родителей необходимо попросить', round(credit - scholarship * 10, 2))
